da22149f369967d70a32c7d480db286aac54bccd
admin <admin@admindeMacBook-Pro-2.local>
2018-08-06 17:31:23 +0800

    first commit

---
+++ FYHire/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FYHire
+//
+//  Created by admin on 2018/8/6.
+//  Copyright © 2018 fanyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'FYHire' do
+	pod 'Moya'
+    pod 'RxSwift'
+    pod 'Kingfisher'
+    pod 'FSPagerView'
+    pod 'SVProgressHUD'
+    pod 'SwiftyUserDefaults', '4.0.0-alpha.1'
+    pod 'IQKeyboardManagerSwift'
+end
